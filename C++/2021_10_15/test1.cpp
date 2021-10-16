bee71bb1201e3a44530015018cd466d7b72f9702
Diogo Santos <diogo_goncalves2004@hotmail.com>
2021-10-16 13:37:50 +0100

    adding more readability to the code

--- C++/2021_10_15/test1.cpp
+++ C++/2021_10_15/test1.cpp
@@ -7,10 +7,12 @@
 #include <winnls.h>
 
 //function headers
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);    //Window procedure function
-void onResize(HWND hwnd, UINT flag, int width, int height);                         //executed when window is resized
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); //Window procedure function
+void onResize(HWND hwnd, UINT flag, int width, int height);                      //executed when window is resized
 
+//WinMain Function Region.
 int WINAPI WinMain(
+#pragma region WinMain();
     _In_ HINSTANCE hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
     _In_ LPSTR lpCmdLine,
@@ -30,6 +32,7 @@ int WINAPI WinMain(
     // Create the window.
 
     HWND hwnd = CreateWindowEx(
+#pragma region WindowsEX;
         0,                           // Optional window styles.
         CLASS_NAME,                  // Window class
         L"Learn to Program Windows", // Window text
@@ -42,6 +45,7 @@ int WINAPI WinMain(
         NULL,      // Menu
         hInstance, // Instance handle
         NULL       // Additional application data
+#pragma endregion WindowsEX;
     );
 
     //Cancel the window procedure if it isnt assigned to a process number.
@@ -53,8 +57,6 @@ int WINAPI WinMain(
     //Show the window
     ShowWindow(hwnd, nCmdShow);
 
-    
-
     // Run the message loop.
     MSG msg = {};
     while (GetMessage(&msg, NULL, 0, 0))
@@ -64,73 +66,81 @@ int WINAPI WinMain(
     }
 
     return 0;
+#pragma endregion WinMain();
 }
 
+//WinProc Region
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
-        case WM_DESTROY:
-        {
-            PostQuitMessage(0);
-            return 0;
-        }
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
 
-        case WM_INPUTLANGCHANGE:
-        {
-            std::cout << lParam << std::endl;
-            std::cout << "Input Changed" << std::endl;
-            break;
-        }
+    case WM_INPUTLANGCHANGE:
+    {
+        std::cout << lParam << std::endl;
+        std::cout << "Input Changed" << std::endl;
+        break;
+    }
 
-        case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
 
-            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
-            EndPaint(hwnd, &ps);
-            break;
-        }
+        EndPaint(hwnd, &ps);
+        break;
+    }
 
-        case WM_SIZE:
-        {
-            int width = LOWORD(lParam);  // Macro to get the low-order word.
-            int height = HIWORD(lParam); // Macro to get the high-order word.
+    case WM_SIZE:
+    {
+        int width = LOWORD(lParam);  // Macro to get the low-order word.
+        int height = HIWORD(lParam); // Macro to get the high-order word.
 
-            // Respond to the message:
-            onResize(hwnd, (UINT)wParam, width, height);
-            break;
-        }
-        
+        // Respond to the message:
+        onResize(hwnd, (UINT)wParam, width, height);
+        break;
+    }
+
         return 0;
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
+//Function to answer to the resize windows message
 void onResize(HWND hwnd, UINT flag, int width, int height)
-{   
-    if(flag == 0 && width == 0 && height == 0) return;
+{
+    if (flag == 0 && width == 0 && height == 0)
+        return;
+
+    system("CLS");
     std::cout << "================================================================" << std::endl;
-    switch(flag)
+
+    switch (flag)
     {
-        case 0: // If the window is resized run the code below
-            {
-                std::cout << "Window has been resized" << std::endl;
-                break;
-            }
-        case 1: // If the window is minimized run the code below.
-            {
-                std::cout << "Window has been minimized" << std::endl;
-                break;
-            }
-        case 2: // If the window is maximized run the code below
-            {
-                std::cout << "Window has been maximized" << std::endl;
-                break;
-            }
+    case 0: // If the window is resized run the code below
+    {
+        std::cout << "Window has been resized" << std::endl;
+        break;
     }
+    case 1: // If the window is minimized run the code below.
+    {
+        std::cout << "Window has been minimized" << std::endl;
+        break;
+    }
+    case 2: // If the window is maximized run the code below
+    {
+        std::cout << "Window has been maximized" << std::endl;
+        break;
+    }
+    }
+
     std::cout << "================================================================" << std::endl;
     std::cout << "New size" << std::endl;
     std::cout << "width: " << width << std::endl;
